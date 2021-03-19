db7178e5fb81b2c1597dd7ceaaca3551477b0b50
David Ryan Alviola <david.r.alviola@accenturefederal.com>
2021-03-18 21:25:01 -0500

    Added utility to split dataframes with continuous targets

--- preparation/prepare_utils.py
+++ preparation/prepare_utils.py
@@ -38,6 +38,43 @@ def split_dataframe(df, stratify_by=None, rand=1414, test_size=.2, validate_size
 
     return train, validate, test
 
+def split_dataframe_continuous_target(df, target, bins=5, rand=1414, test_size=.2, validate_size=.3):
+    """
+    Utility function to create train, validate, and test splits when targeting a continuous variable.
+
+    Generates train, validate, and test samples from a dataframe when targeting a continuous variable.
+    Credit to @ryanorsinger
+
+    Parameters
+    ----------
+    df : DataFrame
+        The dataframe to be split
+    target : str
+        Name of the continuous target variable. Ensures different results of target variable are spread between the samples.
+    test_size : float
+        Ratio of dataframe (0.0 to 1.0) that should be kept for testing sample. Default is 0.2.
+    validate_size: float
+        Ratio of train sample (0.0 to 1.0) that should be kept for validate sample. Default is 0.3.
+    random_stat : int
+        Value provided to create re-produceable results. Default is 1414.
+
+    Returns
+    -------
+    DataFrame
+        Three dataframes representing the training, validate, and test samples
+    """
+    binned_y = pd.cut(df[target], bins=bins, labels=list(range(bins)))
+    df["bins"] = binned_y
+
+    train_validate, test = train_test_split(df, stratify=df["bins"], test_size=test_size, random_state=rand)
+    train, validate = train_test_split(train_validate, stratify=train_validate["bins"], test_size=validate_size, random_state=rand)
+
+    train = train.drop(columns=["bins"])
+    validate = validate.drop(columns=["bins"])
+    test = test.drop(columns=["bins"])
+    
+    return train, test, validate
+
 def nan_null_empty_check(df):
     """
     Utility function that checks for missing values in a dataframe.
